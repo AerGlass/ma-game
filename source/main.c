9e267c0858dcbfccbb351af9634a2a9a0a991b82
AerGlass <86316669+AerGlass@users.noreply.github.com>
2022-04-23 13:01:37 -0500

    that is ugly

--- source/main.c
+++ source/main.c
@@ -21,23 +21,7 @@
 */
 int main(int argc, char **argv) {
 	int a, b;
-	// Pantalla de espera inicializando NitroFS
-	NF_Set2D(0, 0);
-	NF_Set2D(1, 0);	
 	consoleDemoInit();
-	swiWaitForVBlank();
-	printf("Loading...");
-	// Define el ROOT e inicializa el sistema de archivos
-	NF_SetRootFolder("NITROFS");	// Define la carpeta ROOT para usar NITROFS
-
-	// Inicializa el motor 2D
-	NF_Set2D(0, 0);				// Modo 2D_0 en ambas pantallas
-	NF_Set2D(1, 0);
-
-	// Inicializa los fondos tileados
-	NF_InitTiledBgBuffers();	// Inicializa los buffers para almacenar fondos
-	NF_InitTiledBgSys(0);		// Inicializa los fondos Tileados para la pantalla superior
-	NF_InitTiledBgSys(1);		// Iniciliaza los fondos Tileados para la pantalla inferior
 	printf("%d + %d = %d", a, b, intro(a, b));
 	return 0; 
 
